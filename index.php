4069c9255f201e6fcdded235df13368c09caf455
Habahabahababa <jeroenjoene@live.nl>
2016-12-21 12:30:52 +0100

    Query

--- index.php
+++ index.php
@@ -4,3 +4,23 @@ include 'log/log.php';
 ?>
 
 <title>WhattSearch</title>
+
+<?php
+$owner = $_GET['query'];
+$sql = "SELECT id, siteName FROM Site where siteName like '%".$query."%' or domain like '%".$query."%' or description like '%".$query."%';";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+    while($row = $result->fetch_assoc()) {
+        echo "id = ".$row["id"]."<br/>";
+		echo "title = ".$row["title"]."<br/><br/>";
+	}
+} else {
+//echo "Error: " . $query . "<br>" . $conn->error."<br/>";
+	echo "Oh, my! I... I'm terribly sorry. It seems like something has gone wrong.";
+	echo "This is not what you're looking for";
+}
+?>
+
+Please enter a Search Querry
